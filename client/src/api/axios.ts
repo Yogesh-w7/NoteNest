98d08fce5a110c67a9376970bf1b9ec0b4258d74
Yogesh Anil Waghmare <wyogesh703@gmail.com>
2025-10-03 18:52:17 +0530

    Update axios.ts

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,6 +1,19 @@
 import axios from "axios";
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api",
-  withCredentials: true
+  withCredentials: true,
 });
+
+
+api.interceptors.response.use(
+  (response) => {
+    if (response.data && response.data.data !== undefined) {
+      return response.data.data; // return only useful data
+    }
+    return response.data;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default api;
